d6558ac33dd50dd1e6f29617feb76261efa93de4
Jason Leach <jlspursfan@gmail.com>
2019-04-22 21:37:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,32 @@
 def display_board(board)
-
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
+  if !(0..8).include?(index)
+    return false
+  end
 
+  if (board[index] == "X") || (board[index] == "O")
+    return false
+  end
+
+  if ((0..8).include?(index)) && ((board[index] == " ") || (board[index] == "") || (board[index] == nil))
+    return true
+  end
 end
 
-def move(board, index, token = "X")
+def input_to_index(position)
+  index = (position.to_i) - 1
+  return index
+end
 
+def move( board, index, token = "X" )
+
+  board[index] = token
+  return board
 end
